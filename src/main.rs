462261f49367eef8a940c4de0fece75c9f2e4ced
unknown <nanpu@tpc.com.tw>
2024-10-06 21:23:14 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,8 @@ use std::io::copy;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // step 1: login
+    // step 1: login, 必須使用cookie才能維持登入狀態
     let client = reqwest::Client::builder().cookie_store(true).build()?;
-
-    // send POST　request
     let mut login_data = HashMap::new();
     login_data.insert("username", "admin");
     login_data.insert("password", "123");
@@ -22,35 +20,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
-    // step2 : frontpage -> search
-    // note: 登入後，是否可以直接指定date interval後，直接搜尋結果，
-    // 而不必先進到search.html後再搜尋?
-    let response = client
-        .get("http://192.168.1.100/search.html")
-        .send()
-        .await?;
 
-    let html_content = response.text().await?;
-    let document = Html::parse_document(&html_content);
-    println!("{html_content}");
-
-    // step3: custom date interval, search.js
-    // server=0&ch_username=*&stime=s-today&year=2024&month=10&day=06&hour=00&min=00&sec=00&yearend=2024&monthend=10&dayend=06&hourend=23&minend=59&secend=59
-    //  server=0&ch_username=*&stime=s-range&year=2024&month=9&day=06&hour=00&min=00&sec=00&yearend=2024&monthend=10&dayend=06&hourend=23&minend=59&secend=59
+    // step2: custom date interval, search.js
+    // server=0&ch_username=*&stime=s-range&year=2024&month=9&day=06&hour=00&min=00&sec=00&yearend=2024&monthend=10&dayend=06&hourend=23&minend=59&secend=59
     // http://192.168.1.100/wsgi/search post
     let mut form = HashMap::new();
-    form.insert("server", "0");
-    form.insert("ch_username", "*");
+    form.insert("server", "0"); // 0, local machine
+    form.insert("ch_username", "*");    // all channels
     form.insert("stime", "s-range");
     form.insert("year", "2024");
-    form.insert("month","9");
-    form.insert("day", "06");
+    form.insert("month","1");
+    form.insert("day", "31");
     form.insert("hour", "00");
     form.insert("min", "00");
     form.insert("sec", "00");
     form.insert("yearend", "2024");
-    form.insert("monthend","10");
-    form.insert("dayend", "06");
+    form.insert("monthend","1");
+    form.insert("dayend", "31");
     form.insert("hourend", "23");
     form.insert("minend", "59");
     form.insert("secend", "59");
@@ -60,14 +46,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .send()
     .await?;
 
-
     // parse, 要注意搜尋結果會有多頁
-    // 主要分析audio channel no., channel name, start-end time interval
-    let html_content = response.text().await?;
-    let document = Html::parse_document(&html_content);
-    println!("{html_content}");
+    // 5,18399,2024-10-06 00:35:19,8,2024-10-06 00:35:27,i,,0,高調,,;
+    // 6,18400,2024-10-06 17:38:10,5,2024-10-06 17:38:15,i,,0,北 調,,;
+    // 2,16497,2024-01-31 09:02:14,22,2024-01-31 09:02:35,i,,0,64345,,;
+    // 5,16498,2024-01-31 13:19:00,8,2024-01-31 13:19:08,i,,0, 高調,,;
+    // 1,16499,2024-01-31 13:45:49,12,2024-01-31 13:46:01,i,27842,0,21042,,;
+    // each row split by semicolon ;
+    // each col split by comma ,
+    // col: {0: channel, 1:fileid, 2:start datetime, 3:rec_length ,4L end datetime,, 5:call_direction, 
+    // 6: call_phone_number, 7: 0, 8: phone_name, 9: ?, 10: asr_enable}
+    // objPlayList(arrRecord[7], (i + 1), arrRecord[1], arrRecord[0], arrRecord[8], arrRecord[9], arrRecord[2], arrRecord[4], arrRecord[3], arrRecord[5], arrRecord[6], arrRecord[10]);
+    let results = response.text().await?;
+    println!("{results}");
 
     // download
+    // http://192.168.1.100/wsgi/downloadrec?device_id=0&fileid=16499
     // http://192.168.1.100/audio_mnt/000/ch4/20240829/164503.mp3
 
     // 指定輸出資料夾
